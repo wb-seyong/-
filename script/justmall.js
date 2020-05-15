0dd5881629b04bf4a069ec44efb502729ec69a73
wb-seyong <high1707@gmail.com>
2020-05-15 11:12:40 +0900

    이벤트 핸들러 메뉴 예제

--- script/justmall.js
+++ script/justmall.js
@@ -1,7 +1,41 @@
 $(function(){
     tabs();
     popup();
+    gnb();
 });
+function gnb(){
+    //메인메뉴 마우스 오버하면 서브메뉴 모두 부드럽게 펼쳐줘
+    //그리고 마우스 오버한 메인메뉴는 하이라이트를 적용해줘
+    // $("#gnb > li").mouseover(function(){
+    //     $(this).addClass("on"); //추가해
+    //     $(".lnb").show();
+    // });
+    // $("#gnb > li").mouseout(function(){
+    //     $(this).removeClass("on");//제거해
+    //     $(".lnb").hide();
+    // })
+
+    $("#gnb > li").hover(
+        function(){
+            $(this).addClass("on");
+            $(".lnb").show();
+        },
+        function(){
+            $(this).removeClass("on");
+            $(".lnb").hide();
+        }
+    )
+    // $("#gnb > li").on({
+    //     mouseover:function(){
+    //         $(this).addClass("on");
+    //         $(".lnb").show();
+    //     },
+    //     mouseout:function(){
+    //         $(this).removeClass("on");
+    //         $(".lnb").hide();
+    //     }
+    // })
+}
 function tabs(){
     // notice_on();
     // gallery_on();
@@ -24,7 +58,7 @@ function tabs(){
 function popup() {
     popup_open();
     popup_close();
-}
+    }
 function popup_open() {
     $("#notice li:eq(0)").click(function(){
         //console.log("click");
